c6b965c000e2950de6d0789e98786107101b0495
Will F <willfowlds@gmail.com>
2025-04-16 21:47:45 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/vite-app/' : '/',
-})
+  base: './',   // Use relative path for GitHub Pages
+  
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,  // Clears the dist folder before each build
+    rollupOptions: {
+      input: './index.html',
+    }
+  },
+  
+  server: {
+    open: true  // Automatically open browser
+  }
+});
