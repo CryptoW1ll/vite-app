0470d4a9e58f703ac3a50bf93c04df14f36fb89f
Will F <willfowlds@gmail.com>
2025-04-22 10:55:02 +1200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // Use relative path for GitHub Pages
+  base: '/vite-app/', // Base URL for deployment not   base: './',  
   
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,  // Clears the dist folder before each build
-    rollupOptions: {
-      input: './index.html',
-    }
-  },
+  // build: {
+  //   outDir: 'dist',
+  //   emptyOutDir: true,  
+  //   rollupOptions: {
+  //     input: './index.html',
+  //   }
+  // },
   server: {
     open: true  // Automatically open browser
   }
