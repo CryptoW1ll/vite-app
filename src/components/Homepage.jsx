f6b17505a7c8a956f5f0aa4de7c97bd6b1787abe
Will F <willfowlds@gmail.com>
2025-05-05 09:58:43 +1200

    added react-gtm-module

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,12 @@
 import Footer from './Footer';
 import PlanetEarth from './PlanetEarth.jsx';
+import TagManager from 'react-gtm-module';
 
 
 function Homepage() {
+    useEffect(() => {
+        TagManager.initialize({ gtmId: 'GTM-TKLPNP4W' });
+      }, []);
     return (
         <div className="flex flex-col min-h-screen bg-black">
             <main className="flex-grow">
