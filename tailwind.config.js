66ef6f54ab60ef3968c458b1c2a366a7ed6aace2
Will F <willfowlds@gmail.com>
2025-09-29 15:36:30 +1300

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+  ],
+  safelist: [
+    "max-w-2xl",
+    "md:max-w-3xl",
+    "lg:max-w-4xl",
   ],
   theme: {
     extend: {},
